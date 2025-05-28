3f43a54a33d9774ffadae3f493aa154e2681720f
Meridiano <megametallist@gmail.com>
2025-05-28 14:34:22 +0300

    1.15.216 IDs (A)
    PlayerCamera Singleton
    TESDataHandler Singleton

--- include/RE/IDs.h
+++ include/RE/IDs.h
@@ -395,7 +395,7 @@ namespace RE::ID
 
 	namespace PlayerCamera
 	{
-		inline constexpr REL::ID Singleton{ 0 };         // 878523
+		inline constexpr REL::ID Singleton{ 937788 };
 		inline constexpr REL::ID ForceFirstPerson{ 0 };  // 166029
 		inline constexpr REL::ID ForceThirdPerson{ 0 };  // 166031
 		inline constexpr REL::ID SetCameraState{ 0 };    // 166078
@@ -520,7 +520,7 @@ namespace RE::ID
 
 	namespace TESDataHandler
 	{
-		inline constexpr REL::ID Singleton{ 0 };  // 825890
+		inline constexpr REL::ID Singleton{ 937572 };  // 825890
 	}
 
 	namespace TESDeathEvent
